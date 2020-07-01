22e1c483d40e05fdbb97a71ffc79d2f5393de1cf
Anurodh Kumar <anu.ckp.13@gmail.com>
2020-07-02 04:13:11 +0530

    2nd commit

--- tuts.py
+++ tuts.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('layout.html')
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
